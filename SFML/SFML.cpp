bd3e1786e1bc2137cc1eb4592a64157f7ca8ca8a
BerkayBabaoglu <112875768+BerkayBabaoglu@users.noreply.github.com>
2025-04-06 08:54:04 +0300

    Updated

--- SFML/SFML.cpp
+++ SFML/SFML.cpp
@@ -1,41 +1,83 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
-int currentMenu = 0;
-
 using namespace std;
+
+void SelectMenu();
+void Angle();
+void Radian();
+void asRadian();
+void asDegree();
+
+
+int currentMenu = 0;
+int value = 0;
+
 int main()
 {
-
+    SelectMenu();
+    return 0;
 }
 
 void SelectMenu() {
-
     cout << "** Welcome to Angle Calculator! **" << endl;
 
-    cout << "Please enter the number!" << endl;
+    cout << "Please choose an option:" << endl;
+    cout << "1 - Enter angle in degrees" << endl;
+    cout << "2 - Enter angle in radians" << endl;
+    cout << "3 - Convert degrees to radians" << endl;
+    cout << "4 - Convert radians to degrees" << endl;
 
     cin >> currentMenu;
 
-    if (currentMenu != NULL) {
-        switch (currentMenu)
-        {
-        case 1:
-            Angle();
-        case 2:
-            Radian();
-        case 3:
-            asRadian();
-        case 4:
-            asDegree();
-        default:
-            cout << "Wrong number entered!" << endl;
-            break;
-        }
+    switch (currentMenu)
+    {
+    case 1:
+        Angle();
+        break;
+    case 2:
+        Radian();
+        break;
+    case 3:
+        asRadian();
+        break;
+    case 4:
+        asDegree();
+        break;
+    default:
+        cout << "Wrong number entered!" << endl;
+        break;
     }
 }
 
-float Angle() {
-    cout << "Please enter the angle value!" << endl;
+void Angle() {
+    cout << "Please enter the angle in degrees: ";
+    cin >> value;
 
+    sf::Angle angle1 = sf::degrees(value);
+    cout << "You entered: " << angle1.asDegrees() << " degrees" << endl;
+}
+
+void Radian() {
+    cout << "Please enter the angle in radians: ";
+    cin >> value;
+
+    sf::Angle radian1 = sf::radians(value);
+    cout << "You entered: " << radian1.asRadians() << " radians" << endl;
+}
+
+void asRadian() {
+    cout << "Enter degrees to convert to radians: ";
+    cin >> value;
+
+    sf::Angle angle1 = sf::degrees(value);
+    cout << value << " degrees is " << angle1.asRadians() << " radians" << endl;
+}
+
+void asDegree() {
+    cout << "Enter radians to convert to degrees: ";
+    cin >> value;
+
+    sf::Angle radian1 = sf::radians(value);
+    cout << value << " radians is " << radian1.asDegrees() << " degrees" << endl;
 }
